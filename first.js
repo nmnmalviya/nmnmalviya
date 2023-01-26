63a31368b2d1a3dbcec794642a4c242be2055f96
stark <stark@gmail.com>
2023-01-26 19:54:11 +0530

    fix

---
+++ first.js
@@ -0,0 +1,9 @@
+
+    function pic_bulbon() {
+        document.getElementById("demo").src="pic_bulbon.gif"        
+    }
+    function pic_bulboff(params) {
+        document.getElementById("demo").src="pic_bulboff.gif"
+    }
+
+	"hi"
